c303cd222a836f232f6576f18bed641b68551d98
githyin <kmkm9460@gmail.com>
2023-12-14 16:16:03 +0900

    make Money to Coin Converter

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import { useState, useEffect } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [coinPrice, setCoinPrice] = useState();
+  const [inputMoney, setInputMpney] = useState();
+  const onInputMoney = (event) => {
+    setInputMpney(event.target.value);
+  };
+  const onSelectCoin = (event) => {
+    setCoinPrice(parseInt(event.target.value));
+  };
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
@@ -13,16 +21,41 @@ function App() {
       });
   }, []);
   return (
-    <div className={styles.title}>
-      <h1>The Coins! {coins.length}</h1>
-      {loading ? <strong>Loading...</strong> : null}
-      <ul>
-        {coins.map((coin) => (
-          <li>
-            {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price}
-          </li>
-        ))}
-      </ul>
+    <div>
+      <h1 className={styles.title}>
+        The Coins! {loading ? "" : `(${coins.length})`}
+      </h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <div>
+          <select onChange={onSelectCoin}>
+            <option value={1}>--- Select coin ---</option>
+            {coins.map((coin, index) => (
+              <option key={index} value={coin.quotes.USD.price}>
+                {coin.name} ({coin.symbol})
+              </option>
+            ))}
+          </select>
+          <hr />
+          <label htmlFor="inputMoney">Money</label>
+          <input
+            id="inputMoney"
+            placeholder="Please Input Your Money!"
+            value={inputMoney}
+            type="number"
+            onChange={onInputMoney}
+          ></input>
+          <label htmlFor="outputCoin">Coin</label>
+          <input
+            id="outputCoin"
+            placeholder="Waitting your Input"
+            value={inputMoney / coinPrice}
+            onChange={onInputMoney}
+            disabled
+          ></input>
+        </div>
+      )}
     </div>
   );
 }
